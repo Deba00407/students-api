7fdccc8399a6c0691859ab1edb2ff314c448f3a1
Deba00407 <cdebanjan456@gmail.com>
2025-06-13 00:07:24 +0530

    Added graceful shutdown of servers

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Deba00407/students-api/internal/config"
 )
@@ -19,14 +24,38 @@ func main() {
 	})
 
 	// setup server
+	serverStatus := make(chan os.Signal, 1)
+
 	server := http.Server{
 		Addr:    config.ServerPath,
 		Handler: router,
 	}
-	fmt.Println("Server running...")
-	
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Failed to start the server")
+
+	signal.Notify(serverStatus, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("Failed to start the server")
+		}
+	}()
+
+	slog.Info("Server running on:", slog.String("address:", config.ServerPath))
+
+	<-serverStatus
+
+	// Shutdown server gracefully
+
+	slog.Info("Shutting down server...")
+
+	// Incase a process is running, the server waits for the process to end for 5 secs and once it is complete, if any new request appears it is not accepted
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel() // release resources in case the process exits before the time duration set
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server:", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server shutdown successfull")
 }
